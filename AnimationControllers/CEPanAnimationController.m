8cd6ff01a989cc980c2be6748e509ebd1738f6c2
QuanGe <zhang_ru_quan@163.com>
2015-04-09 17:57:40 +0800

    适配多屏幕

--- AnimationControllers/CEPanAnimationController.m
+++ AnimationControllers/CEPanAnimationController.m
@@ -16,14 +16,15 @@
     // Add the toView to the container
     UIView* containerView = [transitionContext containerView];
     [containerView addSubview:toView];
-    toView.frame = CGRectMake(self.reverse ? -160 : 320, toView.frame.origin.y, toView.frame.size.width, toView.frame.size.height);
+    CGFloat screenWidth = ([UIScreen mainScreen].bounds.size.width);
+    toView.frame = CGRectMake(self.reverse ? -screenWidth/2.0 : screenWidth, toView.frame.origin.y, toView.frame.size.width, toView.frame.size.height);
     
     self.reverse ? [containerView sendSubviewToBack:toView] : [containerView bringSubviewToFront:toView];
     
     // animate
     NSTimeInterval duration = [self transitionDuration:transitionContext];
     [UIView animateWithDuration:duration animations:^{
-        fromView.frame = CGRectMake(!self.reverse ? -160 : 320, fromView.frame.origin.y, fromView.frame.size.width, fromView.frame.size.height);
+        fromView.frame = CGRectMake(!self.reverse ? -screenWidth/2.0 : screenWidth, fromView.frame.origin.y, fromView.frame.size.width, fromView.frame.size.height);
         toView.frame = CGRectMake(0, toView.frame.origin.y, toView.frame.size.width, toView.frame.size.height);
     } completion:^(BOOL finished) {
         if ([transitionContext transitionWasCancelled]) {
@@ -32,7 +33,7 @@
         } else {
             // reset from- view to its original state
             [fromView removeFromSuperview];
-            fromView.frame = CGRectMake(!self.reverse ? -160 : 320, fromView.frame.origin.y, fromView.frame.size.width, fromView.frame.size.height);
+            fromView.frame = CGRectMake(!self.reverse ? -screenWidth/2.0 : screenWidth, fromView.frame.origin.y, fromView.frame.size.width, fromView.frame.size.height);
             toView.frame = CGRectMake(0, toView.frame.origin.y, toView.frame.size.width, toView.frame.size.height);
         }
         [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
